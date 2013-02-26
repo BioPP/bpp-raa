3c5230dcda5e0ac978ba53a598ed5c54e0c15fb3
Julien Y. Dutheil <jy.dutheil@gmail.com>
2013-02-26 10:47:47 +0100

    Added generic includes script.

--- genIncludes.py
+++ genIncludes.py
@@ -2,15 +2,15 @@
 
 import os;
 from os.path import *;
+import sys;
 
-for root, dirs, files in os.walk('src/Bpp'):
-    print "Creating generic include file " + str(root) + ".all"
+for root, dirs, files in os.walk(sys.argv[1]):
+    print "-- Creating generic include file: " + str(root) + ".all"
     f = open(str(root) + ".all", 'w')
     head, tail = split(root)
+    files.sort()
     for file in files:
         base, ext = splitext(file)
-        if (ext == ".h"):
+        if (ext == ".h") or (ext == ".all"):
           f.write("#include \"" + tail + "/" + file + "\"\n")
     f.close()
-    if '.svn' in dirs:
-        dirs.remove('.svn')  # don't visit SVN directories
